9dc381ce0e6693500469e1269b9c73b39c298791
Michael Truong <truong.minh.michael@gmail.com>
2023-01-24 12:40:01 -0800

    testing

--- src/axios/moviesController.js
+++ src/axios/moviesController.js
@@ -3,9 +3,9 @@ import { instance, api } from "./axiosConfig"
 export const read_allService = () => instance.get(
     "movies/read-all", 
     null,
-    //{
+    {
     //     headers: {
-            //withCredentials: true,
+            withCredentials: true,
     //     }
-    //}
+    }
 )
