9d19664942f5ffaceb8c735d6cc972cdb6b6b93f
Michael Truong <truong.minh.michael@gmail.com>
2023-02-05 20:39:13 -0800

    fixing bug for protected comp

--- src/ProtectedRoutes.jsx
+++ src/ProtectedRoutes.jsx
@@ -1,13 +1,33 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { Login } from "./pages/Login";
+import { api } from "./axios/axiosConfig";
+import { useState, useEffect, useRef } from "react";
 
-const useAuth = () => {
-    const user = {loggedIn: false};
+const useAuth = async () => {
+    let response;
+    let user;
+    try {
+        response = await api.movies.read_all()
+        user = {loggedIn: true};
+    }
+    catch (e) {
+        user = {loggedIn: false};
+    }
+    console.log(user)
     return user && user.loggedIn;
 }
 
 const ProtectedRoutes = () => {
-    const isAuth = useAuth();
+    const [isAuth, setIsAuth] = useState(null);
+    useEffect(() => {
+        useAuth().then(isLoggedIn => {
+            setIsAuth(isLoggedIn);
+            console.log("HEREEEE")
+            console.log(isLoggedIn)
+        });
+    }, []);
+    console.log("renders")
+    if (isAuth === null) return;
     return isAuth ? <Outlet/> : <Navigate to="/login" />;
 }
 
